b0b8ebb769134d821df4d58a50885d37c25d1cfe
k12amrul <kamrul00islam0@gmail.com>
2024-06-04 14:45:23 +0800

    users  api route created

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const port = process.env.PORT || 5000
 
 
 app.use(cors())
-app.use(cors({
-  origin: ["http://localhost:5173"],
-//   credentials: true
+// app.use(cors({ 
+//   origin: ["http://localhost:5173"],
+// //   credentials: true
 
-}))
+// }))
 app.use(express.json())
 // app.use(cookieParser())
 
@@ -44,6 +44,8 @@ async function run() {
 
     const database = client.db("bmsDB");
     const apartmentsCollection = database.collection("apartments");
+    const usersCollection = database.collection("users");
+    const agreementsCollection = database.collection("agreements");
     // const artsCollection = database.collection("arts");
     // const usersCollection = database.collection("users");
 
@@ -56,14 +58,53 @@ app.get( '/apartments' ,async ( req , res ) => {
 
 })
 
-    app.get('/arts',  async (req, res) => {
-        // console.log(req.cookies.token)
-  
-        const query = artsCollection.find()//.limit(6)
-        const result = await query.toArray()
-        res.send(result)
-  
-      })
+app.get('/agreements' , async  ( req, res ) =>{
+
+
+  const result =await agreementsCollection.find( ).toArray()
+  res.send( result)
+
+})
+
+   
+app.post('/agreements', async ( req ,res ) =>{
+  const agreement = req.body
+console.log( agreement )
+ 
+const query = { email : agreement.email }
+const exitingAgreement = await agreementsCollection.findOne( query)
+
+if( exitingAgreement ){
+  return res.send( { message : 'you already agreement '})
+}
+const result = await agreementsCollection.insertOne( agreement )
+res.send( result )
+
+
+} )
+
+
+app.get('/users', async (req, res) => {
+
+  const result = await usersCollection.find().toArray()
+  res.send(result)
+
+})
+
+app.post('/users', async (req, res) => {
+  const user = req.body
+  const query = { email: user.email }
+  const exitingUser = await usersCollection.findOne(query)
+  if (exitingUser) {
+    return res.send({ message: 'already exiting User' })
+  }
+
+  const result = await usersCollection.insertOne(user)
+
+  res.send(result)
+
+
+})
 
 
 
